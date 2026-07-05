a81b786118f58322150f6e23e665bf4fcfcec423
Fabian Guarascio <guarascio.fabiand@gmail.com>
2026-07-05 17:55:51 -0300

    feat (performance): Added lazyload of a route and also made the fonts be in the project instead of requiring them from a cdn

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { HeroesList } from './features/heroes/components/heroes-list/heroes-list';
-import { About } from './features/about/about/about';
 import { HeroDetail } from './features/heroes/components/hero-detail/hero-detail';
 import { AddHero } from './features/heroes/components/add-hero/add-hero';
 import { Heroes } from './features/heroes/components/heroes/heroes';
-
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'heroes' },
@@ -17,5 +15,8 @@ export const routes: Routes = [
       { path: ':id', component: HeroDetail },
     ],
   },
-  { path: 'about', component: About },
+  {
+    path: 'about',
+    loadComponent: () => import('./features/about/about/about').then((m) => m.About),
+  },
 ];
